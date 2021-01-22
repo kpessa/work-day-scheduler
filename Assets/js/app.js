7a4b1d46e9157283dfdc6b9c47cd636c6014761b
Kurt Pessa <kpessa@gmail.com>
2021-01-22 07:43:39 -0500

    added document.ready

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,85 +1,87 @@
-var container = $('.container');
-var descriptions = new Array(9); // Used to keep track of entered text
+$(document).ready(function () {
+  var container = $('.container');
+  var descriptions = new Array(9); // Used to keep track of entered text
 
-// ! ------------------------------------------------------
-// ! 1.) Assigning current day to the top of the calendar
-// ! ------------------------------------------------------
-$('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
+  // ! ------------------------------------------------------
+  // ! 1.) Assigning current day to the top of the calendar
+  // ! ------------------------------------------------------
+  $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 
-// ! ------------------------------------------------------
-// ! 2.) Creating HTML / View Programmatically
-// !       a.) Time Column
-// !       b.) Description Column
-// !       c.) Save Button Column
-// ! ------------------------------------------------------
-var createTimeColumn = function (hour) {
-  var unit;
-  var displayHour;
-  hour < 12 ? (unit = 'AM') : (unit = 'PM');
-  hour > 12 ? (displayHour = hour - 12) : (displayHour = hour);
+  // ! ------------------------------------------------------
+  // ! 2.) Creating HTML / View Programmatically
+  // !       a.) Time Column
+  // !       b.) Description Column
+  // !       c.) Save Button Column
+  // ! ------------------------------------------------------
+  var createTimeColumn = function (hour) {
+    var unit;
+    var displayHour;
+    hour < 12 ? (unit = 'AM') : (unit = 'PM');
+    hour > 12 ? (displayHour = hour - 12) : (displayHour = hour);
 
-  var colHour = $('<div>')
-    .addClass('col-1 hour')
-    .text(displayHour + unit);
+    var colHour = $('<div>')
+      .addClass('col-1 hour')
+      .text(displayHour + unit);
 
-  return colHour;
-};
+    return colHour;
+  };
 
-var createDescriptionColumn = function (hour) {
-  descriptions = JSON.parse(localStorage.getItem('descriptions')) || new Array(9);
+  var createDescriptionColumn = function (hour) {
+    descriptions = JSON.parse(localStorage.getItem('descriptions')) || new Array(9);
 
-  var textArea = $('<textarea>')
-    .attr('data-hour', hour)
-    .text(descriptions[hour - 9]);
+    var textArea = $('<textarea>')
+      .attr('data-hour', hour)
+      .text(descriptions[hour - 9]);
 
-  var colDesc = $('<div>').addClass('col-10 description').append(textArea);
+    var colDesc = $('<div>').addClass('col-10 description').append(textArea);
 
-  // var presentHour = +moment().format('H');
-  var presentHour = 12; // for testing
+    // var presentHour = +moment().format('H');
+    var presentHour = 12; // for testing
 
-  if (hour < presentHour) {
-    colDesc.addClass('past');
-  } else if (hour === presentHour) {
-    colDesc.addClass('present');
-  } else if (hour > presentHour) {
-    colDesc.addClass('future');
-  }
+    if (hour < presentHour) {
+      colDesc.addClass('past');
+    } else if (hour === presentHour) {
+      colDesc.addClass('present');
+    } else if (hour > presentHour) {
+      colDesc.addClass('future');
+    }
 
-  return colDesc;
-};
+    return colDesc;
+  };
 
-var createSaveColumn = function (hour) {
-  var colSave = $('<div>').addClass('col-1 saveBtn').html(`<i class="fas fa-save" data-hour=${hour}></i>`);
+  var createSaveColumn = function (hour) {
+    var colSave = $('<div>').addClass('col-1 saveBtn').html(`<i class="fas fa-save" data-hour=${hour}></i>`);
 
-  return colSave;
-};
+    return colSave;
+  };
 
-var createRow = function (hour) {
-  var div = $('<div>').addClass('row');
+  var createRow = function (hour) {
+    var div = $('<div>').addClass('row');
 
-  var colHour = createTimeColumn(hour);
-  var colDesc = createDescriptionColumn(hour);
-  var colSave = createSaveColumn(hour);
+    var colHour = createTimeColumn(hour);
+    var colDesc = createDescriptionColumn(hour);
+    var colSave = createSaveColumn(hour);
 
-  div.append(colHour).append(colDesc).append(colSave);
-  return div;
-};
+    div.append(colHour).append(colDesc).append(colSave);
+    return div;
+  };
 
-var createView = function () {
-  for (var hour = 9; hour <= 17; hour++) {
-    container.append(createRow(hour));
-  }
-};
+  var createView = function () {
+    for (var hour = 9; hour <= 17; hour++) {
+      container.append(createRow(hour));
+    }
+  };
 
-createView();
+  createView();
 
-// ! ------------------------------------------------------
-// ! 3.) Handle click events on Save Icons
-// ! ------------------------------------------------------
-$('.container').on('click', 'i', function () {
-  var dataHour = $(this).attr('data-hour');
-  var arrayIndex = dataHour - 9;
-  descriptions[arrayIndex] = $(`textarea[data-hour=${dataHour}]`).val().trim();
+  // ! ------------------------------------------------------
+  // ! 3.) Handle click events on Save Icons
+  // ! ------------------------------------------------------
+  $('.container').on('click', 'i', function () {
+    var dataHour = $(this).attr('data-hour');
+    var arrayIndex = dataHour - 9;
+    descriptions[arrayIndex] = $(`textarea[data-hour=${dataHour}]`).val().trim();
 
-  localStorage.setItem('descriptions', JSON.stringify(descriptions));
+    localStorage.setItem('descriptions', JSON.stringify(descriptions));
+  });
 });
